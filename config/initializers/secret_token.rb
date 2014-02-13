4723a462696bb5e4d69a51d9542b35fec4cb8f69
James Robinson <james.michael.robinson@googlemail.com>
2014-02-13 19:36:36 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LazyCexio::Application.config.secret_key_base = 'afd42aa7d2f13d76a4d978c8fce1ab440f2fce4091aa651bc90c65006af1796f6ec9f6f979c2876e60942f350e19da1845bbee389d89de7697325fd759260bc0'
