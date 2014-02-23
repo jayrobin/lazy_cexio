6deb99253de24536a4cc44bfdd1db07c40126ca4
James Robinson <james.michael.robinson@googlemail.com>
2014-02-23 21:08:48 +0000

    encrypt API secret keys in db

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-	attr_encrypted :secret, key: ENV["SECRET_KEY"], :unless => Rails.env.development?
+	attr_encrypted :secret, key: Rails.env.development? ? 'SECRET_KEY' : ENV['SECRET_KEY']
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
